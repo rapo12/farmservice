8f7c0cea6e157310dd956c83d5f2998499226c29
ayman-az <aymanzahir3@gmail.com>
2023-07-29 17:30:33 +0100

    smartAgri

---
+++ src/main/java/com/uir/smartAgri/SmartAgriApplication.java
@@ -0,0 +1,13 @@
+package com.uir.smartAgri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAgriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAgriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uir/smartAgri/SmartAgriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uir.smartAgri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAgriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
